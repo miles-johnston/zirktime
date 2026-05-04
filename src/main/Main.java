c3171aefe59107a091b049c4c161a5820bdc6e9a
Miles Johnston <108438836+miles-johnston@users.noreply.github.com>
2026-05-03 20:21:56 -0500

    added a main package + a main class.  Within main I added a JFrame window

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame gameWindow = new JFrame();
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setTitle("IDK WHAT THIS GAME IS YET WAHOO!");
+
+        gameWindow.setLocationRelativeTo(null); //this somehow sets the screen to be in the center
+        gameWindow.setVisible(true);
+    }
+}
